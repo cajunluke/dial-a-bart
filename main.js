0491fe73bbeecc8b7c5a6367689917418e4c04eb
Bex Fortin <cajunluke@gmail.com>
2022-11-12 11:03:19 -0800

    Add html and support files

---
+++ main.js
@@ -0,0 +1,14 @@
+const segments = ["R", "C", "K", "AL", "A", "L", "M", "O"]
+
+const lines = [{
+  color: "Orange",
+  segments: ["R", "K", "AL", "A"],
+}];
+
+const stations = {
+};
+
+function main() {
+}
+
+main();
